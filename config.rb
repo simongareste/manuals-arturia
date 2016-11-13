7f26d1cc8f28efdf443a71851aa3be14984bc952
Simon Gareste <simon@gareste.fr>
2016-11-13 18:53:29 +0100

    Rework folders architecture

--- config.rb
+++ config.rb
@@ -1,28 +1,11 @@
+require 'action_view'
+require 'multi_json'
 
-
-
-
-###
-# Page options, layouts, aliases and proxies
-###
-require 'redcarpet_helper'
-# Per-page layout changes:
-#
-# With no layout
 page '/*.xml', layout: false
 page '/*.json', layout: false
 page '/*.txt', layout: false
 
-# With alternative layout
-# page "/path/to/file.html", layout: :otherlayout
-
-# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
-# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
-#  which_fake_page: "Rendering a fake page with a local variable" }
-
-# General configuration
-
-# Reload the browser automatically whenever files change
+# Reload the browser automatically
 configure :development do
   activate :livereload
 end
@@ -30,55 +13,110 @@ end
 set :markdown_engine, :redcarpet
 set :markdown, :tables => true, :autolink => true, :gh_blockcode => true, :fenced_code_blocks => true, with_toc_data: true, space_after_headers: true
 
-###
-# Helpers
-###
-
 helpers do
-  def table_of_contents(resource)
+  class HTMLBlockCode < Redcarpet::Render::HTML
+    include ActionView::Helpers::UrlHelper
+    include MultiJson
+
+    def image(link, title = nil, alt = nil)
+      klass = nil
+
+      begin
+        multi_images = MultiJson.decode(link)
+        output = %(<div class="row">)
+
+        multi_images.each do |image|
+          klass = image[1]['class'] || "col-xs-12"
+          link = image[1]['link'] || nil
+          title = image[1]['title'] || nil
+          alt = image[1]['alt'] || nil
+          output += image_tag(link, :title => title, :alt => alt, :klass => klass)
+        end
+
+        output += "</div>"
+        %(#{output})
+      rescue
+          output = %(<div class="row">)
+          
+         output += image_tag(link, :title => title, :alt => alt, :klass => klass="col-xs-8 col-xs-offset-2")
+      output += "</div>"
+      %(#{output})
+      end
+    end
+
+    def image_tag(link, options = {})
+      %(
+          <div class="#{options[:klass]} img-box">
+            <img src="#{link}" alt="" title="#{options[:title]}" class="img-responsive" />
+            <p>#{options[:alt]}</p>
+          </div>
+      )
+    end
+  end
+
+  def table_of_contents(path)
     content = ""
-    Dir.foreach('source/partials/chapters/') do |file| 
+    chapters_path = "source/#{File.dirname(path)}/chapters"
+    if !Dir.exists? chapters_path
+      puts "no chapter folder - skipping"
+      markdown.render("")
+    end
+    Dir.foreach("#{chapters_path}") do |file|
       next if file == '.' or file == '..'
-      content = content + File.read("source/partials/chapters/#{file}")
+      content = content + File.read("#{chapters_path}/#{file}")
     end
     toc_renderer = Redcarpet::Render::HTML_TOC.new(nesting_level: 3)
     markdown = Redcarpet::Markdown.new(toc_renderer) # nesting_level is optional
     markdown.render(content)
   end
 
-  def all_chapters()
-    content = ""
-    Dir.foreach('source/partials/chapters/') do |file| 
-      next if file == '.' or file == '..'
-      content = content + File.read("source/partials/chapters/#{file}")
+  # This function gets a path, figure out the product sku from it,
+  # then try and find the json data matching, in which it should find the title
+  def get_title(path)
+    product_sku = product_sku(path)
+    json_file = "source/data/#{product_sku}.json"
+    if !File.exists? "#{Dir.getwd}/#{json_file}"
+      puts "no json data - skipping"
+      return ""
     end
-    chapters_renderer = Redcarpet::Render::HTML.new(with_toc_data: true)
-    markdown = Redcarpet::Markdown.new(chapters_renderer, :tables => true) 
-    markdown.render(content)
-    
+
+    file = File.read("source/data/#{product_sku}.json")
+    begin
+      product_json = MultiJson.decode(file)
+      return product_json['title']
+    rescue
+      return ""
+    end
   end
 
-    
-  def picture(file, alt)
-    @html ="<img src='" + file.url + "' alt='" + alt + "'>"
-    @html.html_safe
+  def product_sku(path)
+    path_array = path.split('/')
+    return path_array[1] || ""
   end
-    
-    
+
+  def all_chapters(path)
+    content = ""
+    chapters_path = "source/#{File.dirname(path)}/chapters"
+    if !Dir.exists? chapters_path
+      puts "no chapter folder - skipping"
+      markdown.render("")
+    end
+    Dir.foreach("#{chapters_path}") do |file|
+      next if file == '.' or file == '..'
+      content = content + File.read("#{chapters_path}/#{file}")
+    end
+    chapters_renderer = HTMLBlockCode.new(with_toc_data: true)
+    markdown = Redcarpet::Markdown.new(chapters_renderer, :tables => true, :autolink => true)
+    markdown.render(content)
+
+  end
+
 end
-# Methods defined in the helpers block are available in templates
-# helpers do
-#   def some_helper
-#     "Helping"
-#   end
-# end
 
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
-   activate :minify_css
-set :relative_links, true
-activate :relative_assets
-  # Minify Javascript on build
-   activate :minify_javascript
+  activate :minify_css
+  set :relative_links, true
+  activate :relative_assets
+  activate :minify_javascript
 end
